c2f9e6b8f2da6fa31adbd45424a85a0e329d0858
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-03-31 15:03:09 -0300

    fix:user authentication

--- api/src/Services/AuthService.js
+++ api/src/Services/AuthService.js
@@ -2,16 +2,14 @@ const Users = require('../Models/Users');
 
 class AuthService {
     static async userAuthentication(email, password) {
-        const user = await Users.findOne({email: email});
+        const user = await Users.findOne({where: {email: email}});
 
         if (!user) {
             throw new Error('User not found.');
         }
-
         if (password != user.password) {
             throw new Error('Invalid password.');
         }
-
         return user;
     }
 }
