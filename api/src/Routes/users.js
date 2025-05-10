c7d7d0fb547b287bf51db323b88211c37665356c
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-05-10 17:41:50 -0300

    fix:User list error

--- api/src/Routes/users.js
+++ api/src/Routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.post('/login', require('../Middlewares/Users/Login'), require('../Controllers/Users/Login'));
 router.post('/register', require('../Middlewares/Users/Register'), require('../Controllers/Users/Register'));
+router.post('/list', require('../Middlewares/Users/List'), require('../Controllers/Users/List'));
 
 module.exports = router;
--- api/src/Services/Users/SelectUsersService.js
+++ api/src/Services/Users/SelectUsersService.js
@@ -2,14 +2,14 @@ const Users = require('../../Models/Users');
 const {Op} = require('sequelize');
 
 class SelectUsersService {
-    static async execute(where) {
+    static async execute({where}) {
         try {
             if (where.name) where.name = {name: {[Op.like]: `%${where.name}%`}};
             const result = await Users.findAll({where});
             if (result.length == 0) return "Usuário não encontrada";
             return result;
         } catch(err) {
-            throw new Error(err.message);
+            throw new Error(err.message || "Internal error");
         }
     }
 }
