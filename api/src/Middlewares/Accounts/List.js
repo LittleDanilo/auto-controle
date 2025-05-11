398d4b154220e2422392ccee8d7cd906960b7e7c
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-05-10 21:46:05 -0300

    fix:fix account list

--- api/src/Middlewares/Accounts/List.js
+++ api/src/Middlewares/Accounts/List.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
     if (!req.body.data) req.body.data = {};
-    const {name, type, status} = req.body.data;
-    if (name && String(name).length <= 0) name = "";
+    const {type, status} = req.body.data;
     if (type && !type in ['Interna', 'Externa']) return res.status(200).json({status: 400, error: "Tipo inválido, precisa ser 'Interna' ou 'Externa'."});
     if (status && !status in ['Ativa', 'Inativa', 'Suspensa']) return res.status(200).json({status: 400, error: "Status inválido, precisa ser 'Ativa', 'Inativa' ou 'Suspensa'."});
     next();
