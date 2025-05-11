10d409073a324478724ba35f33f835e00d4acc39
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-05-11 18:55:39 -0300

    feat:user that aren't admin cannot list the admin data.

--- api/src/Controllers/Users/List.js
+++ api/src/Controllers/Users/List.js
@@ -2,7 +2,9 @@ const SelectUsersService = require('../../Services/Users/SelectUsersService');
 
 module.exports = async (req, res) => {
     try {
-        const result = await SelectUsersService.execute({where: req.body.data});
+        let admin = false
+        if (req.body.userID == 1) admin = true;
+        const result = await SelectUsersService.execute(admin, {where: req.body.data});
         return res.status(200).json({status: 200, result: result});
     } catch (err) {
         return res.status(200).json({status: 400, error: err.message});
