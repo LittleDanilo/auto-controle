47bcdf1ca6a39eea22791a6519424bd867aefa5a
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-05-11 12:13:54 -0300

    fix:update validation

--- api/src/Middlewares/Accounts/Update.js
+++ api/src/Middlewares/Accounts/Update.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     if (!req.body) return res.status(200).json({message: "Nada para atualizar."});
     const {fields, id} = req.body.data;
 
-    if (!fields || fields == {}) return res.status(200).json({status: 400, error: "Nada para atualizar."});
+    if (!fields || Object.keys(fields).length == 0) return res.status(200).json({status: 400, error: "Nada para atualizar."});
     if (!id || id <= 0) return res.status(200).json({status: 400, error: "ID inválido."});
     if (fields.name && String(fields.name).length > 255) return res.status(200).json({status: 400, error: 'Nome muito longo.'});
     if (fields.type && fields.type in ['Interna', 'Externa']) return res.status(200).json({status: 400, error: "Tipo inválido, precisa ser 'Interna' ou 'Externa'."});
