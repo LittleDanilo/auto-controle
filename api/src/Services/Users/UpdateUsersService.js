4e139bd556e8ab0f3baab1ae03634a07fb1f9e07
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-05-22 16:56:27 -0300

    fix:add verification if email is not registered

--- api/src/Services/Users/UpdateUsersService.js
+++ api/src/Services/Users/UpdateUsersService.js
@@ -1,10 +1,14 @@
 const Users = require('../../Models/Users');
+const SelectUsers = require("./SelectUsersService")
 
 class UpdateUsersService {
     static async execute({fields}, id) {
         try {
-            const user = await Users.update(fields, {where: {id: id}});
-            return user;
+            let emailExistsQuery = []
+            if (fields.email) emailExistsQuery = await SelectUsers.execute(true, {where: {email: fields.email}})
+            if (emailExistsQuery.length > 0) throw new Error("Esse email já está atribuido a outro usuário.")
+            const userUpdated = await Users.update(fields, {where: {id: id}});
+            return userUpdated;
         } catch (err) {
             throw new Error(err.message || "Internal error.");
         }
