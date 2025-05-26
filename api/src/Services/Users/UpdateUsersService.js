4ca9968e2f0e50c0fd1188d454b59d0039c9cc05
Danilo Feitosa <danilodasilva.feitosa22@gmail.com>
2025-05-25 21:01:36 -0300

    fiX:user update email verification

--- api/src/Services/Users/UpdateUsersService.js
+++ api/src/Services/Users/UpdateUsersService.js
@@ -2,9 +2,8 @@ const Users = require('../../Models/Users');
 
 class UpdateUsersService {
     static async execute({fields}, id) {
-        console.log(fields);
         try {
-            let emailExistsQuery = []
+            let emailExistsQuery = undefined;
             if (fields.email) emailExistsQuery = await Users.findOne({where: {email: fields.email}});
             if (emailExistsQuery && emailExistsQuery.id != id) throw new Error("Esse email já está atribuido a outro usuário.");
             const userUpdated = await Users.update(fields, {where: {id}});
